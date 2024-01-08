7bf6af27568e8d3622420b105ae0b285edb51bce
Evans Ofori <evansgyan7@gmail.com>
2024-01-08 18:51:07 +0000

    Add video, fix ms clarity

--- apps/boardso/app/components/Footer/AuthFooter.tsx
+++ apps/boardso/app/components/Footer/AuthFooter.tsx
@@ -1,11 +1,13 @@
-"use client";
+"use client"
 
-import { UITypography } from "ui";
+import { UITypography } from "ui"
 
 export default function AuthFooter() {
   return (
     <footer className="py-1">
-      <UITypography className="text-center text-xs">Copypright © 2023 Boardso.com</UITypography>
+      <UITypography className="text-center text-xs">
+        Copyright © 2023 - {new Date().getFullYear()} Boardso.com
+      </UITypography>
     </footer>
-  );
+  )
 }
